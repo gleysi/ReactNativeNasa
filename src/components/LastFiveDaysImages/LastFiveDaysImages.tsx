9abf292c2795a7a68f62b278009c22f34afef011
gleysi <sonia.mireya.miranda@gmail.com>
2025-05-30 12:33:18 -0600

    Added LastFiveDaysImage component and PostImage component

---
+++ src/components/LastFiveDaysImages/LastFiveDaysImages.tsx
@@ -0,0 +1,37 @@
+import React, {FC} from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { PostImage as PostImageTypes} from '../../types';
+import PostImage from '../PostImage';
+
+const LastFiveDaysImages: FC<{postImages ?: PostImageTypes[]}> = ({postImages}) => {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.title}> Last 5 Days</Text>
+        <ScrollView style={styles.content}>
+          {
+            // .map is used to iterate over the array of postImages
+            // and render a PostImage component for each item
+            // The key prop is used to give each PostImage a unique identifier
+          postImages?.map(postImage => (
+            <PostImage key={`post-image-${postImage.title}`} {...postImage}/>
+          ))}
+        </ScrollView>
+    </View>
+  );
+};
+
+const styles= StyleSheet.create({
+  container: {
+    flex: 1,
+    marginVertical: 8,
+  },
+  content: {
+    paddingHorizontal: 24,
+  },
+  title: {
+    color: 'white',
+    fontSize: 16,
+    marginBottom: 18,
+  },
+});
+export default LastFiveDaysImages;
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,10 +4,13 @@ import Header from '../components/Header';
 import TodaysImage from '../components/TodaysImage';
 import fecthApi from '../utils/fetch';
 import { PostImage } from '../types';
+import { format, sub } from 'date-fns';
+import LastFiveDaysImages from '../components/LastFiveDaysImages';
 
 const Home = () => {
   // local state constant
   const [todaysImage, setTodaysImage] = useState<PostImage>();
+  const [last5DaysImages, setLast5DaysImages] = useState<PostImage[]>([]);
   // useEffect means that this function will run after the component mounts
   useEffect(() => {
     const loadTodaysImage = async () => {
@@ -20,13 +23,27 @@ const Home = () => {
       }
     };
 
+    const loadLast5DaysImages = async () => {
+      try {
+        const date = new Date();
+        const todaysDate = format(date, 'yyyy-MM-dd');
+        const fiveDaysAgoDate = format(sub(date, {days:5}) , 'yyyy-MM-dd');
+        const lastFaveDaysResponse = await fecthApi(`&start_date=${fiveDaysAgoDate}&end_date=${todaysDate}`);
+        setLast5DaysImages(lastFaveDaysResponse);
+      } catch (error) {
+        console.error('Error loading last 5 days images:', error);
+      }
+    };
+
     loadTodaysImage().catch(null);
+    loadLast5DaysImages().catch(null);
   }, []); // Second argument is an empty array of dependencies, meaning this effect runs only once when the component mounts.
 
   return (
     <View style={styles.container}>
       <Header />
       <TodaysImage {...todaysImage}/>
+      <LastFiveDaysImages postImages={last5DaysImages} />
     </View>
   );
 };
