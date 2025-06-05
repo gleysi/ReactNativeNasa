46417bde0dbdd34475b1951312c6dbcd074f95e7
gleysi <sonia.mireya.miranda@gmail.com>
2025-06-05 15:36:52 -0600

    Added NestJS

--- src/components/TodaysImage/TodaysImage.tsx
+++ src/components/TodaysImage/TodaysImage.tsx
@@ -1,14 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Image, Button} from 'react-native';
 import { PostImage } from '../../types';
 
 // Functional component type definition
 const TodaysImage: FC<PostImage> = ({date, title, url}) => {
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://192.168.4.84:3000/welcome')
+      .then(res => res.text())
+      .then(setMessage)
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={{ uri: url }} style={styles.image} />
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.date}>{date}</Text>
+      <Text style={styles.nest}>{message}</Text>
       <View style={styles.buttonContainer}><Button title="View" /></View>
     </View>
   );
@@ -36,6 +47,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   date: {
+    color: 'white',
+    fontSize: 16,
+  },
+  nest: {
     color: 'white',
     fontSize: 16,
   },
